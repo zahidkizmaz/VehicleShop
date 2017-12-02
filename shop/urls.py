ef4dd265f5fb87a374b2c9d052d57cd2b6223298
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-12-03 00:08:34 +0300

    url bug fixed..

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from shop.views import CategoryView , HomePageView 
 
 urlpatterns = [
-        url(r'^', HomePageView.as_view(), name="home"),        
+        url(r'^$', HomePageView.as_view(), name="home"),        
         url(r'^category$', CategoryView.as_view(), name="categorylist"), 
         
         ]
