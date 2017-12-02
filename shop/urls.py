16dd59f0be30f59bcbb5381c56534ff56192a55a
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-12-02 19:18:11 +0300

    homepage view added,vehilelist html created vehicles listed in list view..

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from shop.views import CategoryView
+from shop.views import CategoryView , HomePageView 
 
 urlpatterns = [
-                url(r'^category$', CategoryView.as_view(), name="categorylist"), 
+        url(r'^', HomePageView.as_view(), name="home"),        
+        url(r'^category$', CategoryView.as_view(), name="categorylist"), 
         
         ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, Vehicle
 from django.views import generic
 
 class CategoryView(generic.ListView):
@@ -11,4 +11,14 @@ class CategoryView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["categorylist"] = self.get_queryset()
+        return context
+
+class HomePageView(generic.ListView):
+
+    def get_queryset(self):
+        return Vehicle.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["vehiclelist"] = Vehicle.objects.all()
         return context
