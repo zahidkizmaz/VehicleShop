58bbf459c24ec136b35be45bb495baef4101b4fe
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-12-02 16:14:09 +0300

    category view added to views.py, urls.py created in shop/ templates folder created

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category
+from django.views import generic
+
+class CategoryView(generic.DetailView)
+
+    def get_queryset(self):
+         return Category.objects.all()
+     
