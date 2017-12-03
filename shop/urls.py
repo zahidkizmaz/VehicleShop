72cc626c4530849858404c6ebf7916c365b2fb2a
Hatice Erdirik <erdirik.hatice121@gmail.com>
2017-12-03 12:34:07 +0300

    firmlist view added to views and url created

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from shop.views import CategoryView , HomePageView 
+from shop.views import CategoryView , HomePageView, FirmView
 
 urlpatterns = [
         url(r'^', HomePageView.as_view(), name="home"),        
-        url(r'^category$', CategoryView.as_view(), name="categorylist"), 
-        
-        ]
+        url(r'^category$', CategoryView.as_view(), name="categorylist"),
+        url(r'^firm$',FirmView.as_view(), name="firmlist"),
+      ]
