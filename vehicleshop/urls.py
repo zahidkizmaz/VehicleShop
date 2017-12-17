2c7e67d384c411151d4ad9b310ee85ca41dccaa1
harunglec <harunglec35@gmail.com>
2017-12-18 00:51:16 +0300

    Logout button changed

--- vehicleshop/urls.py
+++ vehicleshop/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name = 'login'),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name = 'logout'),
+    url(r'^accounts/logout/$', auth_views.logout,{'next_page': '/'}, name = 'logout'),
     url(r'^', include(shop_urls)),
     ]
