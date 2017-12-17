72a87576171b6171fc16fbc4a3642ad31cb27f11
Zahid KIZMAZ <mzkizmaz@gmail.com>
2017-12-17 15:21:05 +0300

    logout button functionality added

--- vehicleshop/urls.py
+++ vehicleshop/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name = 'login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name = 'logout'),
     url(r'^', include(shop_urls)),
     ]
